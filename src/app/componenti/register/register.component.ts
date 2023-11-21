935fef6bec7e1055877c372d3139737240cf8fb0
Dmytriy Sharov <dima.sharov99.ds@gmail.com>
2023-11-21 17:46:59 +0100

    fix registrazione

--- src/app/componenti/register/register.component.ts
+++ src/app/componenti/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ApiService } from 'src/app/service/api.service';
@@ -8,9 +9,10 @@ import { ApiService } from 'src/app/service/api.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  urlUser: any = 'https://gorest.co.in/public/v2/users';
   idUser: any;
   persone: any;
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private htp: HttpClient) {}
   ngOnInit(): void {
     if (this.idUser == null) {
       this.idUser = 'Registrati per scoprirlo';
@@ -21,14 +23,13 @@ export class RegisterComponent implements OnInit {
     const email = form.value.email;
     const gender = form.value.gender;
     const token: string = form.value.token;
-    localStorage.setItem('token', token);
-    this.apiService
-      .addUser({
-        name: name,
-        email: email,
-        gender: gender,
-        status: 'active',
-      })
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    const httpOption = { headers: headers };
+    console.log(form.value);
+    this.htp
+      .post(this.urlUser, { name, email, gender, status: 'active' }, httpOption)
       .subscribe((data: any) => {
         console.log(data);
         this.apiService.createUser(
@@ -38,7 +39,8 @@ export class RegisterComponent implements OnInit {
           data.status
         );
         localStorage.setItem('user', JSON.stringify(this.apiService.user));
-        this.apiService.getUser().subscribe((data) => {
+        localStorage.setItem('token', token);
+        this.htp.get(this.urlUser, httpOption).subscribe((data) => {
           this.persone = data;
           if (this.idUser == null) {
             this.idUser = 'Registrati per scoprirlo';
